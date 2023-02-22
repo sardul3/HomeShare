354c6012528906bd260b7a46514b15ede60dfd39
sagar <poudelsagar530@gmail.com>
2023-02-21 18:59:46 -0600

    first commit

---
+++ src/main/java/com/sagarpoudel/eurekadiscoveryservice/EurekaDiscoveryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sagarpoudel.eurekadiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaDiscoveryServiceApplication.class, args);
+    }
+
+}
